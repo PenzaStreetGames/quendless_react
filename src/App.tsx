25f66acc4a436655aa0bdfda9fce06111f6203b9
Pavel Solomatin <crave_ozer_man@mail.ru>
2023-04-12 01:40:12 +0300

    (feat) translate from task-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
+import MainPage from "./main_page";
+import {styles} from "./styles"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={styles.MainPageBackground}>
+        <div className="container min-vh-100 d-flex flex-column justify-content-center align-content-center">
+          <MainPage>
+          </MainPage>
+        </div>
+      </div>
   );
 }
 
---
+++ src/styles.tsx
@@ -0,0 +1,12 @@
+const MainPageBackground = {
+    backgroundImage: "url('https://catherineasquithgallery.com/uploads/posts/2021-03/1614724550_14-p-foni-dlya-lendinga-14.jpg')",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+};
+
+export const styles = {
+    MainPageBackground: MainPageBackground,
+};
+
+export default styles;
