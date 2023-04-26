a3bd76ef9b2d79d6f7f7631a33ed5542c05a7835
Pavel Solomatin <crave_ozer_man@mail.ru>
2023-04-27 01:52:25 +0300

    (feat) add base layout of Queues and Groups pages

--- src/components/pages/GroupsPage.tsx
+++ src/components/pages/GroupsPage.tsx
@@ -1,7 +1,30 @@
+import {ContentWrapper} from "../primitives/ContentWrapper";
+import {Panel} from "../primitives/Panel";
+import {Group, GroupCard} from "../cards/GroupCard";
+
+function getGroups(): Group[] {
+    return [
+        {
+            "name": "IKBO-01-20"
+        },
+        {
+            "name": "IPPO"
+        }
+    ]
+}
+
 export function GroupsPage() {
+    const groupCards = getGroups().map((group) => <GroupCard group={group}/>)
     return (
-        <div>
-            <p>This is Groups' page</p>
-        </div>
+        <ContentWrapper>
+            <Panel>
+                <h2>Groups</h2>
+                {groupCards.map((groupCard) => (
+                    <div>
+                        {groupCard}
+                    </div>
+                ))}
+            </Panel>
+        </ContentWrapper>
     )
 }
